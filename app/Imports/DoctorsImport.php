07aefc6b76b7f91a7c5fb9236d167789fea761ce
soheib5566 <soheib5566@gmail.com>
2024-02-17 22:32:23 +0200

    Add Excel Columns

--- app/Imports/DoctorsImport.php
+++ app/Imports/DoctorsImport.php
@@ -8,14 +8,21 @@ use Maatwebsite\Excel\Concerns\ToModel;
 class DoctorsImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new Doctor([
             //
+            'ID' => $row['ID'],
+            'Lat' => $row['Lat'],
+            'Long' => $row['Long'],
+            'Description' => $row['Description'],
+            'Rate' => $row['Rate'],
+            'Governorate' => $row['Governorate'],
+            'Phone' => $row['Phone'],
         ]);
     }
 }
