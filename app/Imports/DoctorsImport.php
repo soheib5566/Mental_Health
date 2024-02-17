c6ab9267f6ef5f0e87c452182106f72b4f078f9b
soheib5566 <soheib5566@gmail.com>
2024-02-17 22:23:38 +0200

    create DoctorsImport

---
+++ app/Imports/DoctorsImport.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Doctor;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class DoctorsImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Doctor([
+            //
+        ]);
+    }
+}
