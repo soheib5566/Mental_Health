8c78517021602121673edabf1f85d8a9d51e0704
soheib5566 <soheib5566@gmail.com>
2024-03-23 06:06:14 +0200

    commit message

--- database/migrations/2024_03_20_105503_add_phone_to_users_table.php
+++ database/migrations/2024_03_20_105503_add_phone_to_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->dropColumn('location');
+            $table->string('phone')->unique()->nullable();
         });
     }
 
