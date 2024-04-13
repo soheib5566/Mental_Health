18594368279fa8614c31592f7c9c672b80813fd1
soheib5566 <soheib5566@gmail.com>
2024-04-13 12:58:04 +0200

    change AdminController name

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\admincontroller;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DoctorController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
